b0df933278441493cdc981c3704d94bde124fc3b
Vince Saenz <vincesaenz89@gmail.com>
2026-02-25 15:31:53 -0600

    code added

--- challenge.js
+++ challenge.js
@@ -1,11 +1,31 @@
-// vato
-console.log('test')
-console.log('another test')
 
-let firstName
-let age
-let annualIncome
-let creditScore
-let isCitizen
-let hasCriminalRecord
 
+let firstName = "Vince"
+let age = 36
+let annualIncome = 100000
+let creditScore = 720
+let isCitizen = true
+let hasCriminalRecord = false
+
+let accountApproval
+
+if (age > 18 && isCitizen && !hasCriminalRecord) {
+    accountApproval = true
+    console.log("Account Approved");
+} else {
+    accountApproval = false
+    console.log("Account Denied");
+}
+
+
+if (annualIncome > 100000 && creditScore >= 720 && accountApproval) {
+    console.log("Premium Approved")
+}   else {
+    console.log("Standard Account Only")
+}
+
+if (creditScore < 600 || annualIncome < 25000) {
+    console.log("Flag for Review")
+}   else {
+    console.log("No Risk Flags")
+}
